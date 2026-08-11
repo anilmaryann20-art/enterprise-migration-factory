7f4c60f7095bd6240632d3e803651bfe8d636cf7
Maryann <anilmaryann20@gmail.com>
2026-08-11 22:09:43 +0530

    added  Azure Log analytics

--- terraform/modules/log_analytics/main.tf
+++ terraform/modules/log_analytics/main.tf
@@ -4,5 +4,5 @@ resource "azurerm_log_analytics_workspace" "this" {
   resource_group_name = var.resource_group_name
 
   sku               = "PerGB2018"
-  retention_in_days = 30
+  retention_in_days = 31
 }
